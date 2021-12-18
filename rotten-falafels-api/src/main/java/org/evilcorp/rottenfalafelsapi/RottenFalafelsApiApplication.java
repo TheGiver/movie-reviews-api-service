cce742b048e6d2a0c9e0c60d5ed4fc2914e16e8c
TheGiver <faysalkharraz@gmail.com>
2021-12-18 21:39:53 +0200

    chore: preparing a basic spring boot application to link to the swagger documentation created.

---
+++ rotten-falafels-api/src/main/java/org/evilcorp/rottenfalafelsapi/RottenFalafelsApiApplication.java
@@ -0,0 +1,13 @@
+package org.evilcorp.rottenfalafelsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RottenFalafelsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RottenFalafelsApiApplication.class, args);
+    }
+
+}
