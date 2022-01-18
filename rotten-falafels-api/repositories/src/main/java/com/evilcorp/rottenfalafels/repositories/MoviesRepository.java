14ba218b4ae2222929703f6f0dd3cbdfaf02b289
TheGiver <faysalkharraz@gmail.com>
2022-01-18 18:37:18 +0200

    chore: added the necessary methods to query the movies relation.

--- rotten-falafels-api/repositories/src/main/java/com/evilcorp/rottenfalafels/repositories/MoviesRepository.java
+++ rotten-falafels-api/repositories/src/main/java/com/evilcorp/rottenfalafels/repositories/MoviesRepository.java
@@ -4,7 +4,15 @@ import com.evilcorp.rottenfalafels.entities.movies.MoviesEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface MoviesRepository extends CrudRepository<MoviesEntity, Long> {
 
+    Optional<List<MoviesEntity>> getAllMoviesAndTheirReviews();
+
+    Optional<MoviesEntity> getMovieAndItsReviewByMovieTitle(String title);
+
+    Optional<MoviesEntity> getAvgRatingByMovieTitle(String title);
 }
